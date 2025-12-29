a75531f97414bee94b8993360a32bcee64d92864
Rheno Julius Andreas <rhenojulius@gmail.com>
2025-12-29 18:01:46 +0700

    update: titel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,23 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Admin Dashboard</title>
+    <title>
+        @if(Request::is('SKM*'))
+            Survei Kepuasan Masyarakat
+        @elseif(Request::is('superadmin*'))
+            Superadmin - RSD Kalisat
+        @elseif(Request::is('admin*'))
+            Admin - RSD Kalisat
+        @else
+            Login - RSD Kalisat
+        @endif
+    </title>
     <link rel="icon" href="{{ asset('image/Logo-RS.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @yield('styles')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 <body class="@yield('body-class')">
     <header class="site-header">
         <a href="
